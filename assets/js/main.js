df175da1e419277947f106b5fac5f90c41150c12
marcolivierkeekae-droid <marcolivierkeekae@gmail.com>
2025-10-27 21:10:55 +0200

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": {"value": 80},
+    "size": {"value": 3},
+    "move": {"speed": 1},
+    "line_linked": {"enable": true, "color": "#00ffff"},
+    "color": {"value": "#00ffff"}
